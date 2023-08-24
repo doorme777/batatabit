2af952d89ad757515f5434c3e17cbf1fe98427a3
doorme777 <137806586+doorme777@users.noreply.github.com>
2023-08-24 09:46:44 -0600

    fix: se arreglo la animación de las tablas.

--- main.js
+++ main.js
@@ -17,11 +17,25 @@ function incrementNumber() {
 }
 
 function moveRight() {
+  monedas.style.removeProperty("animation");
+
+  move.style.right = "none";
+  move.style.left = "2rem";
+  move.style.transform = "rotate(180deg)";
+
   comiciones.style.order = 1;
+  comiciones.style.animation = "aparecer 0.3s ease-in-out";
+
   monedas.style.order = 2;
 }
 
 function moveLeft() {
+  move.style.removeProperty("left");
+  move.style.removeProperty("transform");
+  comiciones.style.removeProperty("animation");
+
   comiciones.style.order = 2;
   monedas.style.order = 1;
+
+  monedas.style.animation = "irse 0.3s ease-in-out";
 }
