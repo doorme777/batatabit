1e7023cf500fec7159e2ce5483e7fbd134afc98c
doorme777 <137806586+doorme777@users.noreply.github.com>
2023-08-25 10:48:23 -0600

    fixed: section de tablas de comisiones, y monedas

--- main.js
+++ main.js
@@ -19,36 +19,36 @@ if (window.innerWidth >= 765) {
 //   scrollPlanes.style.removeProperty(" scroll-snap-type ");
 // }
 
-function incrementNumber() {
-  if (concurrent === 0) {
-    ++concurrent;
-    moveRight();
-  } else {
-    --concurrent;
-    moveLeft();
-  }
-}
+// function incrementNumber() {
+//   if (concurrent === 0) {
+//     ++concurrent;
+//     moveRight();
+//   } else {
+//     --concurrent;
+//     moveLeft();
+//   }
+// }
 
-function moveRight() {
-  monedas.style.removeProperty("animation");
+// function moveRight() {
+//   monedas.style.removeProperty("animation");
 
-  move.style.right = "none";
-  move.style.left = "2rem";
-  move.style.transform = "rotate(180deg)";
+//   move.style.right = "none";
+//   move.style.left = "2rem";
+//   move.style.transform = "rotate(180deg)";
 
-  comiciones.style.order = 1;
-  comiciones.style.animation = "aparecer 0.3s ease-out";
+//   comiciones.style.order = 1;
+//   comiciones.style.animation = "aparecer 0.3s ease-out";
 
-  monedas.style.order = 2;
-}
+//   monedas.style.order = 2;
+// }
 
-function moveLeft() {
-  move.style.removeProperty("left");
-  move.style.removeProperty("transform");
-  comiciones.style.removeProperty("animation");
+// function moveLeft() {
+//   move.style.removeProperty("left");
+//   move.style.removeProperty("transform");
+//   comiciones.style.removeProperty("animation");
 
-  comiciones.style.order = 2;
-  monedas.style.order = 1;
+//   comiciones.style.order = 2;
+//   monedas.style.order = 1;
 
-  monedas.style.animation = "irse 0.3s ease-out";
-}
+//   monedas.style.animation = "irse 0.3s ease-out";
+// }
